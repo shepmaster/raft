c5049aebf662b5cbae3d63af053fc849bc5705b5
Jake Goulding <jake.goulding@gmail.com>
2014-01-26 11:59:06 -0500

    Improve reporting of tests that fail due to a timeout

--- test/raft/full_stack_test.clj
+++ test/raft/full_stack_test.clj
@@ -4,17 +4,34 @@
             [raft.state.register :as register]
             [raft.full-stack :refer :all]))
 
-(defn wait-for
-  "Wait for a condition to become truthy. Returns `false` if the
-  timeout expires."
-  [timeout-ms f]
-  (if (>= timeout-ms 0)
-    (if-let [result (f)]
-      result
-      (do
-        (Thread/sleep 50)
-        (recur (- timeout-ms 50) f)))
-    false))
+(defn eventually=*
+  "Wait for a function to equal the expected value. Returns a tuple of
+  :success/:timeout and the actual value."
+  [timeout-ms expected f]
+  (let [result (f)
+        remaining-timeout (- timeout-ms 50)]
+    (cond
+     (= expected result) [:success result]
+     (<= remaining-timeout 0) [:timeout result]
+     :else (do
+             (Thread/sleep (min remaining-timeout 50))
+             (recur remaining-timeout expected f)))))
+
+(defmethod assert-expr 'eventually= [msg form]
+  `(let [timeout# ~(nth form 1)
+         expected# ~(nth form 2)
+         actual-fn# (fn [] ~(nth form 3))]
+     (let [[result# actual#] (eventually=* timeout# expected# actual-fn#)]
+       (if (= :success result#)
+         (do-report {:type :pass,
+                     :message ~msg,
+                     :expected expected#,
+                     :actual actual#})
+         (do-report {:type :fail,
+                     :message ~msg,
+                     :expected expected#,
+                     :actual (str actual# " (after waiting " timeout# " ms)")}))
+       (= :success result#))))
 
 (deftest full-stack
   (let [state (register/create)
@@ -23,15 +40,15 @@
         started (start servers)]
     (try
       (testing "There is only one leader"
-        (is (wait-for 300 #(= 1 (count (leaders started))))))
+        (is (eventually= 300 1 (count (leaders started)))))
       (let [terms (each-server started :term)]
         (testing "All the servers are on the same term"
-          (is (wait-for 300 #(= 1 (count (set terms))))))
+          (is (eventually= 300 1 (count (set terms)))))
         (testing "It only takes one term to establish a leader"
-          (is (wait-for 300 #(= 1 (first terms))))))
+          (is (eventually= 300 1 (first terms)))))
       (testing "A user command is committed in reasonable time"
         (let [command-finished (raft/user-command* (leader started) [:set {:name :a, :to 5}])]
           (is (deref command-finished 300 false))
-          (is (= 5 (:a @state)))))
+          (is (eventually= 300 5 (:a @state)))))
       (finally
         (stop started)))))
